7f1e1d44809de7208567961244e41126a7c0b31d
gibbo101 <lukegibson101@gmail.com>
2022-11-24 23:33:18 +0000

    create

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function() {
+
+  $user = User::findOrFail(1);
+  $post = new Post(['title'=>'My first post with Edwin Diaz', 'body'=>'I love laravel']);
+
+  $user->posts()->save($post);
+
+  return "Done";
+
 });
